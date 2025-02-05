40bc6ae7652073e0ff1aaaa2bf184daa07e2b61a
bopeep934 <gkinfn@gmail.com>
2025-02-05 22:56:35 +0900

    34번

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution s=new Solution();
+        System.out.println(s.solution("Zbcdefg"	));
+        }
+
+    }
